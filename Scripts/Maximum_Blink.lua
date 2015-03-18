9b6c17c2f9e6fb7525eb5a951d28db92d584e722
RatchyZanko <ratchapong.t@gmail.com>
2015-03-18 02:09:57 -0500

    Version 1.0a

--- Scripts/Maximum_Blink.lua
+++ Scripts/Maximum_Blink.lua
@@ -11,7 +11,10 @@
         
     Change log:
     ----------
-        Version 1.0 - 24th February 2015 3:18PM:
+        Version 1.0a - 18th March 2015 1:20AM:
+            - User can now choose whether the hot key should include ALT key
+            - User can now choose whether the script will operate as quickcast or not (Thanks Nova)
+        Version 1.0 - 18th March 2015 3:18PM:
             - Initial release.
 ]]--
 require("libs.ScriptConfig")
@@ -19,13 +22,46 @@ require("libs.Utils")
 
 config = ScriptConfig.new()
 config:SetParameter("BlinkRange", true)
-config:SetParameter("BlinkKey", 87, config.TYPE_HOTKEY) -- "W"
+config:SetParameter("AltKey", true)
+config:SetParameter("Quickcast", false)
+config:SetParameter("BlinkKey", 65, config.TYPE_HOTKEY) -- "A"
 config:Load()
 Blink_Range = config.BlinkRange
+Qckcast = config.Quickcast
 Blink_Key = config.BlinkKey
+ALT_Key = config.AltKey
+
 local rangeEffect = {}
-local keyPress = {}
+local blinkHotKey = Blink_Key
+local ALT = ALT_Key
+local isQuickCast = Qckcast
+local keyPressed = nil
+
+function Key(msg, code)
+    if ALT then
+        keyPressed = IsKeyDown(18) and code == blinkHotKey
+    else
+        keyPressed = code == blinkHotKey
+    end
+    if keyPressed then
+        aiming = true  --Now I know that the user is aiming with the spell
+        if isQuickCast == true then
+            active = true
+        end
+    end
+
+    if aiming and msg == RBUTTON_DOWN then --If I right click to cancel or move
+        aiming = false
+    end
+
+    if aiming and msg == LBUTTON_DOWN then --If I left click to use the spell 
+        active = true
+        return true -- Wont go through dota
+    end
+end
+
 function Tick(tick)
+    
     currentTick = tick
     if not client.connected or client.loading or not PlayingGame() or client.console or not SleepCheck("stop") then return end
     if sleepTick and sleepTick > tick then return end
@@ -43,8 +79,17 @@ function Tick(tick)
             collectgarbage("collect")
         end
     end
-
-    if IsKeyDown(Blink_Key) then
+    --[[for i = 1,6 do
+        local item = me:GetItem(i)
+        if item ~= nil then
+            if item.name == "item_blink" then
+                local e = client:GetKeyBinding("dota_item_execute " .. i - 1)
+                print(e)
+            end
+        end
+        
+    end]]
+    if active then
         local blink = me:FindItem("item_blink")
         if blink then
             local distance = math.sqrt(math.pow(client.mousePosition.x - me.position.x, 2) + math.pow(client.mousePosition.y - me.position.y, 2))
@@ -57,6 +102,8 @@ function Tick(tick)
                 else
                     me:CastAbility(blink, Vector(client.mousePosition.x, client.mousePosition.y, 0))
                 end
+                aiming = false
+                active = false
             end
         end
     end
@@ -76,3 +123,4 @@ end
 
 script:RegisterEvent(EVENT_TICK,Tick)
 script:RegisterEvent(EVENT_CLOSE,GameClose)
+script:RegisterEvent(EVENT_KEY,Key)
